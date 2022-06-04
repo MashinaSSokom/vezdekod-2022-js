79436ff1a04936e4f5753ffb5dcdf3e20ed4face
MashinaSSokom <skltan@mail.ru>
2022-06-04 16:46:15 +0800

    user bonus score setter

--- src/components/BonustStore/BonusProducts.vue
+++ src/components/BonustStore/BonusProducts.vue
@@ -5,9 +5,12 @@
       <br/>
       {{ bonusValue }}
     </div>
-    <input type="number" v-model="bonusValue"/>
-    <button
-      class="text-center px-4 py-2 rounded-md transition-all shadow-md ring-2 font-semibold text-white bg-orange-500 ring-orange-500 active:bg-orange-400 active:ring-orange-400">
+    <input type="number" v-model="bonusInputValue"/>
+    <button class="text-center px-4 py-2 rounded-md transition-all shadow-md ring-2
+    font-semibold text-white bg-orange-500 ring-orange-500 active:bg-orange-400 active:ring-orange-400"
+            @click="setBonusValue"
+
+    >
       Добавить
     </button>
   </div>
@@ -19,8 +22,14 @@ export default {
   data() {
     return {
       bonusValue: 0,
+      bonusInputValue: 0,
     };
   },
+  methods: {
+    setBonusValue() {
+      this.bonusValue = Number.isInteger(+this.bonusInputValue)?this.bonusInputValue:this.bonusValue
+    }
+  }
 }
 </script>
 
